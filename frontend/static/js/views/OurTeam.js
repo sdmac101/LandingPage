42a12f9d757c052928bb4bec2d7c4e0c6eecf6e1
Tamhid Chowdhury <tc5g@mtmail.mtsu.edu>
2020-11-24 11:30:37 -0600

    more update

--- frontend/static/js/views/OurTeam.js
+++ frontend/static/js/views/OurTeam.js
@@ -45,7 +45,142 @@ export default class extends AbstractView {
                 </div>
                 
             </div>
+
         </section>
+
+
+        <div class = "pImg1">
+            <div class = "pText1">
+                <span class = "border1">
+                    Executive Committee
+                </span>
+            </div>
+        </div>
+            
+        <div class="card-group">
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Jennings H. Jones</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Board of Directors President</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">District Attorney General</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Terry A. Fann</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Vice President</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Tammy Greer, CPA</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Treasurer</h6>
+                </div>
+            </div>
+            
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Catherine M. Post</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Secretary</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Elizabeth Renegar Parker</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Nominating Committee Chair person</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Tracy D. Sheppard</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Fundraising Committee Chair</h6>
+                </div>
+            </div>
+        </div>
+
+        <div class = "pImg1">
+            <div class = "pText1">
+                <span class = "border1">
+                    Board Members
+                </span>
+            </div>
+        </div>
+
+        <div class="card-group">
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Taminko Amuzu</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Department of Children’s Services</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Chief Kevin Arnold</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Smyrna Police Department</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Chief Michael Bowen</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Murfreesboro Police Department</h6>
+                </div>
+            </div>
+            
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Dr. Tammy Garrett</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Murfreesboro City Schools</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Aurelia Holden</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Charity Circle Representative</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Emily Hutchinson</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Junior League Representative</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Katie Jordan</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Charity Circle Representative</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Deirdre C. Lackey</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Tennessee Office of Child Welfare Reform</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">Betsy Murfree</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Junior League Representative</h6>
+                </div>
+            </div>
+
+            <div class="card" style="width: 18rem; text-align: center;">
+                <div class="card-body">
+                    <h4 class="card-title">William C. Whitesell, Jr.</h4>
+                    <h6 class="card-subtitle mb-2 text-muted">Retired District Attorney General</h6>
+                </div>
+            </div>
+        </div>
         `;
     }
 }
