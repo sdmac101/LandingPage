392b4e9707b14cbdd17a5ce7eaf76bccc6f8a9bf
sdmac101 <32308258+sdmac101@users.noreply.github.com>
2020-11-24 23:35:32 -0600

    added rutherford services html

--- frontend/static/js/views/RCServices.js
+++ frontend/static/js/views/RCServices.js
@@ -9,7 +9,52 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1> Testing the Rutherford Services Page </h1>
+            <div class = "RCImg">
+                <h1>Rutherford County</h1>   
+            </div>
+
+            <h1 id="services-header"> Services </h1>
+
+
+            <div class="service-block">
+                <div class="block-item">
+                    <h1>Forensic Interview</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, vel mollitia. Neque quod alias expedita nam deleniti earum dolorum beatae voluptatem, vitae corporis laudantium natus, repellat ex dicta eum ullam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, recusandae? Quia, praesentium molestias repellat, optio rerum magni iure minus debitis aperiam accusamus ab quam iste, corporis ullam harum unde quis!</p>
+                </div>
+                <div class="block-item">
+                    <img src="static/css/Miscimages/forensic-services-image.jpg" width="440px">
+                </div>
+            </div>
+
+            <div class="service-block">
+                <div class="block-item">
+                    <img src="static/css/Miscimages/family-services-image.jpg" width="440px">
+                </div>
+                <div class="block-item">
+                    <h1>Family Services</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, vel mollitia. Neque quod alias expedita nam deleniti earum dolorum beatae voluptatem, vitae corporis laudantium natus, repellat ex dicta eum ullam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, recusandae? Quia, praesentium molestias repellat, optio rerum magni iure minus debitis aperiam accusamus ab quam iste, corporis ullam harum unde quis!</p>
+                </div>
+            </div>
+
+            <div class="service-block">
+                <div class="block-item">
+                    <h1>Drug Endangered Children Program</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, vel mollitia. Neque quod alias expedita nam deleniti earum dolorum beatae voluptatem, vitae corporis laudantium natus, repellat ex dicta eum ullam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, recusandae? Quia, praesentium molestias repellat, optio rerum magni iure minus debitis aperiam accusamus ab quam iste, corporis ullam harum unde quis!</p>
+                </div>
+                <div class="block-item">
+                    <img src="static/css/Miscimages/drug-endangered-children-program-image.jpg" width="440px" height="300px">
+                </div>
+            </div>
+
+            <div class="service-block">
+                <div class="block-item">
+                    <img src="static/css/Miscimages/bilingual-services-image.jpg" width="440px">
+                </div>
+                <div class="block-item">
+                    <h1>Bilingual Services</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, vel mollitia. Neque quod alias expedita nam deleniti earum dolorum beatae voluptatem, vitae corporis laudantium natus, repellat ex dicta eum ullam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio, recusandae? Quia, praesentium molestias repellat, optio rerum magni iure minus debitis aperiam accusamus ab quam iste, corporis ullam harum unde quis!</p>
+                </div>
+            </div>
         `;
     }
 }
